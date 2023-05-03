cebbc6042c4a16739baca9b2fb23725f7e4e5ff5
BELIVABLE-Pedro <jorge12flor@gmail.com>
2023-05-03 11:24:51 -0300

    alisa meu java

--- java/aprovação.java
+++ java/aprovação.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class aprovação {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+   
+   System.out.println("numero de faltas:");
+   int faltas=sc.nextInt();
+   System.out.println("coloque sua média de notas: ");
+   double media =sc.nextDouble();
+   
+   if((media<10 || media>6)  || faltas== 5 ){System.out.println("O aluno foi aprovado.");}
+   
+
+   else{System.out.println("o aluno foi reprovado. ");}
+    sc.close();}
 }
